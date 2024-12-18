0e12771803c6e23e0f5d7730dc4a30d047b0e911
Rishabh2482 <rishabh2002chauhan@gmail.com>
2024-12-18 17:47:41 +0530

    Added some extra features(-> Added text and completed key in the todoList key of the loaddata object) and manged one bug due to adding that feature ,added some buttons features.

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
 // & Below function load all the todo items present in localstorage and return all the todo in form of object as all the todo are present in string form
 function loaddata(key){
     const data=JSON.parse(localStorage.getItem(key))||{"todoList":[]};
-    // console.log(data);
     return data;
 }
 
 // & Below function pushes the todo items into the array of the object returned by the loaddata function.
-function addtoDoItem(items){
+function addtoDoItem(todoText){
     const data = loaddata('todo');
-    data.todoList.push(items);
+    data.todoList.push({
+        text:todoText,
+        completed:false,
+    });
     localStorage.setItem("todo",JSON.stringify(data));
 }
 
 //& Append the items into the unordered list.
-function appendChildInList(taskList,value)
+function appendChildInList(value)
 {
-    let elment=document.createElement("li")
-    elment.textContent=value;
-    taskList.appendChild(elment);
+    const taskList=document.getElementById("taskList");
+
+    let element=document.createElement("li")
+    element.textContent=value;
+
+    // let editbtn=document.createElement("button");
+    // let deletebtn=document.createElement("button");
+    // let completedbtn=document.createElement("button");
+
+    // editbtn.textContent="Edit";
+    // deletebtn.textContent="Delete";
+    // completedbtn.textContent="Completed";
+
+    // editbtn.classList.add("editbutton");
+    // deletebtn.classList.add("deletebutton");
+    // completedbtn.classList.add("completedbutton");
+
+    // element.appendChild(editbtn);
+    // element.appendChild(deletebtn);
+    // element.appendChild(completedbtn);
+
+    taskList.appendChild(element);
 }
 
 // & this is a eventlistner which is called only after all the DOM content is loaded on the page.
@@ -30,13 +51,14 @@ document.addEventListener("DOMContentLoaded",()=>{
 
     const list=document.getElementById("taskList");
 
+
     //& Below code writes the output of all the changes on the console.
     input.addEventListener("change",(e)=>{
         const text=e.target.value;
 
         e.target.value=text.trim();
 
-        console.log(e.target.value);
+        // console.log(e.target.value);
     })
 
     // & Below code manges the event that what to do after clicking the button
@@ -48,8 +70,8 @@ document.addEventListener("DOMContentLoaded",()=>{
             alert('Please Enter Some value');
         }
         else{
-            addtoDoItem(inputToDo.value);
-            appendChildInList(list,todovalue);
+            addtoDoItem(todovalue);
+            appendChildInList(todovalue);
             inputToDo.value='';
         }
     })
@@ -58,12 +80,12 @@ document.addEventListener("DOMContentLoaded",()=>{
     //& What is use case of below code.
     //! Below code insert all the todo items in the ul tag as page refreshes so that no previous todo are lost.
     const todos = loaddata('todo');
-    console.log(todos);
+    // console.log(todos);
 
     todos["todoList"].forEach(todo => {
         const newTodoItem = document.createElement("li");
-        newTodoItem.textContent = todo;
+        newTodoItem.textContent = todo.text;
         list.appendChild(newTodoItem);
     })
 });
-// console.log("Js Loaded");
+
